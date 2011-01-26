3bf5c51f89ad16a6f7aed7ef4aa655d1dd06d991
ivanhoegarin <ivanhoegarin@10.23.19.154>
2011-01-27 06:27:47 +0800

    MyCommit3

---
+++ src/com/labojava/supinface/android/Group.java
@@ -0,0 +1,16 @@
+package com.labojava.supinface.android;
+
+public class Group {
+	
+	int id;
+	String type;
+	String value;
+	int user_Id;
+	int user_group_Id;
+	
+	Group(int aID)
+	{
+		this.id= aID;
+	}
+
+}
---
+++ src/com/labojava/supinface/android/Skill.java
@@ -0,0 +1,15 @@
+package com.labojava.supinface.android;
+
+public class Skill {
+	
+	int id;
+	String label;
+	String rate;
+	int user_Id;
+	
+	Skill(int aID)
+	{
+		this.id= aID;
+	}
+
+}
