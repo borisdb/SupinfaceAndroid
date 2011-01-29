b0b77cd3a1c307531e69db600c715d1703e3fc6a
Gaël Lamborelle <112350@supinfo.com>
2011-01-29 18:01:04 -0500

    GL - Test super commit de la mort qui tue !

--- src/com/labojava/supinface/android/MainActivity.java
+++ src/com/labojava/supinface/android/MainActivity.java
@@ -7,6 +7,7 @@ public class MainActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
+    	// Moi aussi  je fais des super tests ! 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
